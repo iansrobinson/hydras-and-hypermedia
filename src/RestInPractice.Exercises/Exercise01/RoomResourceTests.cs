34b76bf04c420c9a269601c547afbfd71f879d31
Ian Robinson <iansrobinson@gmail.com>
2011-07-31 12:18:59 +0100

    Added first RoomResource test

---
+++ src/RestInPractice.Exercises/Exercise01/RoomResourceTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Net.Http;
+using NUnit.Framework;
+using RestInPractice.Server.Resources;
+
+namespace RestInPractice.Exercises.Exercise01
+{
+    [TestFixture]
+    public class RoomResourceTests
+    {
+        [Test]
+        public void Returns200Ok()
+        {
+            var resource = new RoomResource();
+            var response = resource.Get("1", new HttpRequestMessage());
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
---
+++ src/RestInPractice.Server/Resources/RoomResource.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.ServiceModel.Syndication;
+using System.ServiceModel.Web;
+using Microsoft.ApplicationServer.Http;
+
+namespace RestInPractice.Server.Resources
+{
+    public class RoomResource
+    {
+        [WebGet]
+        public HttpResponseMessage<SyndicationFeed> Get(string id, HttpRequestMessage request)
+        {
+            return new HttpResponseMessage<SyndicationFeed>(HttpStatusCode.OK);
+        }
+    }
+}
