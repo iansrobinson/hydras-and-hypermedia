dbe98c0f883b123e5d0605e0361a52cd1f52fa4f
iansrobinson <iansrobinson@gmail.com>
2011-08-02 15:07:35 +0100

    Exercise 2 functional test now checks path

--- src/RestInPractice.Exercises/Exercise02/Part03_FunctionalTests.cs
+++ src/RestInPractice.Exercises/Exercise02/Part03_FunctionalTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using Microsoft.ApplicationServer.Http.Activation;
@@ -18,6 +20,22 @@ namespace RestInPractice.Exercises.Exercise02
         [Test]
         public void ShouldNavigateMaze()
         {
+            var expectedPath = new[]
+                                   {
+                                       "http://localhost:8081/rooms/1",
+                                       "http://localhost:8081/rooms/4",
+                                       "http://localhost:8081/rooms/7",
+                                       "http://localhost:8081/rooms/6",
+                                       "http://localhost:8081/rooms/5",
+                                       "http://localhost:8081/rooms/4",
+                                       "http://localhost:8081/rooms/8",
+                                       "http://localhost:8081/rooms/9",
+                                       "http://localhost:8081/rooms/8",
+                                       "http://localhost:8081/rooms/10"
+                                   };
+
+            var path = new List<string>();
+
             var configuration = HttpHostConfiguration.Create()
                 .SetResourceFactory((type, instanceContext, request) => new RoomResource(Maze.Instance), (instanceContext, obj) => { })
                 .AddFormatters(AtomMediaType.Formatter);
@@ -37,11 +55,12 @@ namespace RestInPractice.Exercises.Exercise02
                     state = state.NextState(httpClient);
                     if (state.GetType().Equals(typeof (Exploring)))
                     {
-                        Console.WriteLine(state.CurrentResponse.RequestMessage.RequestUri.AbsoluteUri);
+                        path.Add(state.CurrentResponse.RequestMessage.RequestUri.AbsoluteUri);
                     }
                 }
 
                 Assert.IsInstanceOf(typeof (GoalAchieved), state);
+                Assert.IsTrue(path.SequenceEqual(expectedPath));
 
                 host.Close();
             }
