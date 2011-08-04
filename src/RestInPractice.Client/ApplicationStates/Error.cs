3c95a99306abe7c5abf07d38233c718935815e6f
iansrobinson <iansrobinson@gmail.com>
2011-08-04 23:06:52 +0100

    Added Error ApplicationState

---
+++ src/RestInPractice.Client/ApplicationStates/Error.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace RestInPractice.Client.ApplicationStates
+{
+    public class Error : IApplicationState
+    {
+        private readonly HttpResponseMessage currentResponse;
+        private readonly IEnumerable<Uri> history;
+
+        public Error(HttpResponseMessage currentResponse, IEnumerable<Uri> history)
+        {
+            this.currentResponse = currentResponse;
+            this.history = history;
+        }
+
+        public IApplicationState NextState(HttpClient client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public HttpResponseMessage CurrentResponse
+        {
+            get { return currentResponse; }
+        }
+
+        public IEnumerable<Uri> History
+        {
+            get { return history; }
+        }
+
+        public bool IsTerminalState
+        {
+            get { return true; }
+        }
+    }
+}
