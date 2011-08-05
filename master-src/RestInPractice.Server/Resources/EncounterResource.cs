b68fa70ab2cb0bf9e98d6ef00ee5b14167ef043d
iansrobinson <iansrobinson@gmail.com>
2011-08-05 17:48:59 +0100

    Started developing and testing EncounterResource

---
+++ master-src/RestInPractice.Server/Resources/EncounterResource.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.ServiceModel.Syndication;
+using Microsoft.ApplicationServer.Http;
+using RestInPractice.MediaTypes;
+
+namespace RestInPractice.Server.Resources
+{
+    public class EncounterResource
+    {
+        public HttpResponseMessage<SyndicationFeed> Get(string id, HttpRequestMessage request)
+        {
+            var body = new SyndicationFeed();
+
+            var response = new HttpResponseMessage<SyndicationFeed>(body) { StatusCode = HttpStatusCode.OK };
+            response.Headers.CacheControl = new CacheControlHeaderValue {NoCache = true, NoStore = true};           
+            response.Content.Headers.ContentType = AtomMediaType.Feed;
+
+            return response;
+        }
+    }
+}
---
+++ src/RestInPractice.Exercises/Exercise03/Part01_EncounterResourceTests.cs
@@ -0,0 +1,71 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.ServiceModel.Syndication;
+using NUnit.Framework;
+using RestInPractice.MediaTypes;
+using RestInPractice.Server.Resources;
+
+namespace RestInPractice.Exercises.Exercise03
+{
+    [TestFixture]
+    public class Part01_EncounterResourceTests
+    {
+        private const string RequestUri = "http://localhost:8081/encounters/1";
+
+        [Test]
+        public void ShouldReturn200OK()
+        {
+            var resource = CreateResourceUnderTest();
+            var response = resource.Get("1", CreateRequest());
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Test]
+        public void ResponseShouldIndicateItMustBeRevalidatedWithOriginServer()
+        {
+            var resource = CreateResourceUnderTest();
+            var response = resource.Get("1", CreateRequest());
+
+            Assert.IsTrue(response.Headers.CacheControl.NoCache);
+        }
+
+        [Test]
+        public void ResponseShouldIndicateItMustNotBeStoredByCaches()
+        {
+            var resource = CreateResourceUnderTest();
+            var response = resource.Get("1", CreateRequest());
+
+            Assert.IsTrue(response.Headers.CacheControl.NoStore);
+        }
+
+        [Test]
+        public void ResponseShouldIncludeAtomFeedContentTypeHeader()
+        {
+            var resource = CreateResourceUnderTest();
+            var response = resource.Get("1", CreateRequest());
+
+            Assert.AreEqual(AtomMediaType.Feed, response.Content.Headers.ContentType);
+        }
+
+        [Test]
+        public void ResponseContentShouldBeSyndicationFeed()
+        {
+            var resource = CreateResourceUnderTest();
+            var response = resource.Get("1", CreateRequest());
+            var body = response.Content.ReadAsOrDefault();
+
+            Assert.IsInstanceOf(typeof (SyndicationFeed), body);
+        }
+
+        private static EncounterResource CreateResourceUnderTest()
+        {
+            return new EncounterResource();
+        }
+
+        private static HttpRequestMessage CreateRequest()
+        {
+            return new HttpRequestMessage(HttpMethod.Get, RequestUri);
+        }
+    }
+}
