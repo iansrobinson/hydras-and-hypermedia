88aa383a1b7804e1102da1de7bb3dfc518669cde
iansrobinson <iansrobinson@gmail.com>
2011-08-11 17:53:10 +0100

    Refactored resolving encounter tests to make them easier to understand

--- src/RestInPractice.Exercises/Exercise03/Part02_ResolvingEncounterResourceTests.cs
+++ src/RestInPractice.Exercises/Exercise03/Part02_ResolvingEncounterResourceTests.cs
@@ -8,7 +8,6 @@ using System.ServiceModel.Syndication;
 using Microsoft.ApplicationServer.Http.Dispatcher;
 using NUnit.Framework;
 using RestInPractice.Client.Comparers;
-using RestInPractice.Exercises.Helpers;
 using RestInPractice.MediaTypes;
 using RestInPractice.Server.Domain;
 using RestInPractice.Server.Resources;
@@ -18,16 +17,15 @@ namespace RestInPractice.Exercises.Exercise03
     [TestFixture]
     public class Part02_ResolvingEncounterResourceTests
     {
-        private static readonly Encounter Encounter = Monsters.NewInstance().Get(1);
         private static readonly KeyValuePair<string, string> ClientEndurance = new KeyValuePair<string, string>("endurance", "10");
-        private const string RequestUri = "http://localhost:8081/encounters/1";
         private const string InvalidEncounterId = "999";
 
         [Test]
         public void ShouldReturn201Created()
         {
-            var resource = CreateResourceUnderTest();
-            var response = resource.Post("1", CreateRequest(CreateFormUrlEncodedContent(ClientEndurance)));
+            var encounter = CreateEncounter();
+            var resource = CreateEncounterResource(encounter);
+            var response = resource.Post(encounter.Id.ToString(), CreateRequest(encounter.Id, CreateFormUrlEncodedContent(ClientEndurance)));
 
             Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
         }
@@ -35,10 +33,13 @@ namespace RestInPractice.Exercises.Exercise03
         [Test]
         public void ShouldReturnLocationHeaderWithUriOfNewlyCreatedResource()
         {
-            var resource = CreateResourceUnderTest();
-            var response = resource.Post("1", CreateRequest(CreateFormUrlEncodedContent(ClientEndurance)));
+            var encounter = CreateEncounter();
+            var resource = CreateEncounterResource(encounter);
+            var response = resource.Post(encounter.Id.ToString(), CreateRequest(encounter.Id, CreateFormUrlEncodedContent(ClientEndurance)));
 
-            Assert.AreEqual(new Uri("http://localhost:8081/encounters/1/round/2"), response.Headers.Location);
+            var expectedUri = new Uri(string.Format("http://localhost:8081/encounters/{0}/round/{1}", encounter.Id, encounter.GetAllRounds().Last().Id));
+
+            Assert.AreEqual(expectedUri, response.Headers.Location);
         }
 
         [Test]
@@ -46,8 +47,9 @@ namespace RestInPractice.Exercises.Exercise03
         {
             try
             {
-                var resource = CreateResourceUnderTest();
-                resource.Post(InvalidEncounterId, CreateRequest(CreateFormUrlEncodedContent(ClientEndurance)));
+                var encounter = CreateEncounter();
+                var resource = CreateEncounterResource(encounter);
+                resource.Post(InvalidEncounterId, CreateRequest(encounter.Id, CreateFormUrlEncodedContent(ClientEndurance)));
                 Assert.Fail("Expected 404 Not Found");
             }
             catch (HttpResponseException ex)
@@ -59,8 +61,9 @@ namespace RestInPractice.Exercises.Exercise03
         [Test]
         public void ResponseShouldIncludeAtomEntryContentTypeHeader()
         {
-            var resource = CreateResourceUnderTest();
-            var response = resource.Post("1", CreateRequest(CreateFormUrlEncodedContent(ClientEndurance)));
+            var encounter = CreateEncounter();
+            var resource = CreateEncounterResource(encounter);
+            var response = resource.Post(encounter.Id.ToString(), CreateRequest(encounter.Id, CreateFormUrlEncodedContent(ClientEndurance)));
 
             Assert.AreEqual(AtomMediaType.Entry, response.Content.Headers.ContentType);
         }
@@ -68,8 +71,9 @@ namespace RestInPractice.Exercises.Exercise03
         [Test]
         public void ResponseContentShouldBeSyndicationItem()
         {
-            var resource = CreateResourceUnderTest();
-            var response = resource.Post("1", CreateRequest(CreateFormUrlEncodedContent(ClientEndurance)));
+            var encounter = CreateEncounter();
+            var resource = CreateEncounterResource(encounter);
+            var response = resource.Post(encounter.Id.ToString(), CreateRequest(encounter.Id, CreateFormUrlEncodedContent(ClientEndurance)));
             var item = response.Content.ReadAsOrDefault();
 
             Assert.IsInstanceOf(typeof (SyndicationItem), item);
@@ -78,8 +82,9 @@ namespace RestInPractice.Exercises.Exercise03
         [Test]
         public void ItemShouldContainRoundCategory()
         {
-            var resource = CreateResourceUnderTest();
-            var response = resource.Post("1", CreateRequest(CreateFormUrlEncodedContent(ClientEndurance)));
+            var encounter = CreateEncounter();
+            var resource = CreateEncounterResource(encounter);
+            var response = resource.Post(encounter.Id.ToString(), CreateRequest(encounter.Id, CreateFormUrlEncodedContent(ClientEndurance)));
             var item = response.Content.ReadAsOrDefault();
 
             Assert.IsTrue(item.Categories.Contains(new SyndicationCategory("round"), CategoryComparer.Instance));
@@ -88,49 +93,62 @@ namespace RestInPractice.Exercises.Exercise03
         [Test]
         public void ItemIdShouldBeTagUri()
         {
-            var resource = CreateResourceUnderTest();
-            var response = resource.Post("1", CreateRequest(CreateFormUrlEncodedContent(ClientEndurance)));
+            var encounter = CreateEncounter();
+            var resource = CreateEncounterResource(encounter);
+            var response = resource.Post(encounter.Id.ToString(), CreateRequest(encounter.Id, CreateFormUrlEncodedContent(ClientEndurance)));
             var item = response.Content.ReadAsOrDefault();
-            
-            Assert.AreEqual("tag:restinpractice.com,2011-09-05:/encounters/1/round/2", item.Id);
+
+            var expectedItemId = string.Format("tag:restinpractice.com,2011-09-05:/encounters/{0}/round/{1}", encounter.Id, encounter.GetAllRounds().Last().Id);
+
+            Assert.AreEqual(expectedItemId, item.Id);
         }
 
         [Test]
-        public void ItemShouldHaveASelfLink()
+        public void ItemShouldHaveSelfLink()
         {
-            var resource = CreateResourceUnderTest();
-            var response = resource.Post("1", CreateRequest(CreateFormUrlEncodedContent(ClientEndurance)));
+            var encounter = CreateEncounter();
+            var resource = CreateEncounterResource(encounter);
+            var response = resource.Post(encounter.Id.ToString(), CreateRequest(encounter.Id, CreateFormUrlEncodedContent(ClientEndurance)));
             var item = response.Content.ReadAsOrDefault();
             var selfLink = item.Links.First(l => l.RelationshipType.Equals("self"));
 
-            Assert.AreEqual(new Uri("http://localhost:8081/encounters/1/round/2"), selfLink.Uri);
+            var expectedUri = new Uri(string.Format("http://localhost:8081/encounters/{0}/round/{1}", encounter.Id, encounter.GetAllRounds().Last().Id));
+
+            Assert.AreEqual(expectedUri, selfLink.Uri);
         }
 
         [Test]
-        public void ItemTitleShouldBeRound2()
+        public void ItemTitleShouldRepresentCurrentRound()
         {
-            var resource = CreateResourceUnderTest();
-            var response = resource.Post("1", CreateRequest(CreateFormUrlEncodedContent(ClientEndurance)));
+            var encounter = CreateEncounter();
+            var resource = CreateEncounterResource(encounter);
+            var response = resource.Post(encounter.Id.ToString(), CreateRequest(encounter.Id, CreateFormUrlEncodedContent(ClientEndurance)));
             var item = response.Content.ReadAsOrDefault();
+
+            var expectedTitle = "Round " + encounter.GetAllRounds().Last().Id;
             
-            Assert.AreEqual("Round 2", item.Title.Text);
+            Assert.AreEqual(expectedTitle, item.Title.Text);
         }
 
         [Test]
         public void ItemSummaryShouldIndicateRemainingMonsterEndurance()
         {
-            var resource = CreateResourceUnderTest();
-            var response = resource.Post("1", CreateRequest(CreateFormUrlEncodedContent(ClientEndurance)));
+            var encounter = CreateEncounter();
+            var resource = CreateEncounterResource(encounter);
+            var response = resource.Post(encounter.Id.ToString(), CreateRequest(encounter.Id, CreateFormUrlEncodedContent(ClientEndurance)));
             var item = response.Content.ReadAsOrDefault();
 
-            Assert.AreEqual("The " + Encounter.Title + " has 6 Endurance Points", item.Summary.Text);
+            var expectedSummary = string.Format("The {0} has {1} Endurance Points", encounter.Title, encounter.GetAllRounds().Last().Endurance);
+            
+            Assert.AreEqual(expectedSummary, item.Summary.Text);
         }
 
         [Test]
         public void ItemContentShouldContainXhtml()
         {
-            var resource = CreateResourceUnderTest();
-            var response = resource.Post("1", CreateRequest(CreateFormUrlEncodedContent(ClientEndurance)));
+            var encounter = CreateEncounter();
+            var resource = CreateEncounterResource(encounter);
+            var response = resource.Post(encounter.Id.ToString(), CreateRequest(encounter.Id, CreateFormUrlEncodedContent(ClientEndurance)));
             var item = response.Content.ReadAsOrDefault();
 
             Assert.AreEqual("xhtml", item.Content.Type);
@@ -139,36 +157,44 @@ namespace RestInPractice.Exercises.Exercise03
         [Test]
         public void ItemContentShouldIncludeFormContainingRemainingClientEndurance()
         {
-            const string @expectedXhtml = @"<div xmlns=""http://www.w3.org/1999/xhtml"">
-  <form action=""/encounters/1"" method=""POST"" enctype=""application/x-www-form-urlencoded"">
-    <input type=""text"" name=""endurance"" value=""9"" />
+            var encounter = CreateEncounter();
+            var resource = CreateEncounterResource(encounter);
+            var response = resource.Post(encounter.Id.ToString(), CreateRequest(encounter.Id, CreateFormUrlEncodedContent(ClientEndurance)));
+            var item = response.Content.ReadAsOrDefault();
+            var xhtml = (TextSyndicationContent) item.Content;
+
+            var @expectedXhtml = string.Format(@"<div xmlns=""http://www.w3.org/1999/xhtml"">
+  <form action=""/encounters/{0}"" method=""POST"" enctype=""application/x-www-form-urlencoded"">
+    <input type=""text"" name=""endurance"" value=""{1}"" />
     <input type=""submit"" value=""Submit"" />
   </form>
-</div>";
-            
-            var resource = CreateResourceUnderTest();
-            var response = resource.Post("1", CreateRequest(CreateFormUrlEncodedContent(ClientEndurance)));
-            var item = response.Content.ReadAsOrDefault();
-            var xhtml = (TextSyndicationContent)item.Content;
+</div>", encounter.Id, int.Parse(ClientEndurance.Value) - 1);
 
             Assert.AreEqual(expectedXhtml, xhtml.Text);
         }
 
-        private static EncounterResource CreateResourceUnderTest()
+        private static EncounterResource CreateEncounterResource(Encounter encounter)
         {
-            return new EncounterResource(Monsters.NewInstance());
+            return new EncounterResource(new Repository<Encounter>(encounter));
         }
 
-        private static HttpRequestMessage CreateRequest(HttpContent content)
+        private static HttpRequestMessage CreateRequest(int encounterId, HttpContent content)
         {
-            var request = new HttpRequestMessage {Method = HttpMethod.Post, RequestUri = new Uri(RequestUri), Content = content};
-            request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+            var requestUri = new Uri("http://localhost:8081/encounters/" + encounterId);
+            var request = new HttpRequestMessage {Method = HttpMethod.Post, RequestUri = requestUri, Content = content};
             return request;
         }
 
         private static HttpContent CreateFormUrlEncodedContent(params KeyValuePair<string, string>[] parameters)
         {
-            return new FormUrlEncodedContent(parameters);
+            var content = new FormUrlEncodedContent(parameters);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+            return content;
+        }
+
+        private static Encounter CreateEncounter()
+        {
+            return new Encounter(1, "Monster", "Encounter description", 2, 1, 8);
         }
     }
 }
