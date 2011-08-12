bf28bef0dcfc624ff17935e2fe86a597c07dcf0a
iansrobinson <iansrobinson@gmail.com>
2011-08-12 09:06:40 +0100

    Created some tests around returning encounters for rooms

--- src/Tests.RestInPractice/Server/Domain/RoomTests.cs
+++ src/Tests.RestInPractice/Server/Domain/RoomTests.cs
@@ -9,21 +9,47 @@ namespace Tests.RestInPractice.Server.Domain
         [Test]
         public void ShouldReturnEncounter()
         {
+            var encounter = CreateUnresolvedEncounter();
+            var room = CreateRoomWithEncounter(encounter.Id);
+
+            var returnedEncounter = room.GetEncounter(new Repository<Encounter>(encounter));
+
+            Assert.AreEqual(encounter, returnedEncounter);
         }
 
         [Test]
         public void ShouldIndicatedItIsNotGuardedIfThereIsNoEncounter()
         {
+            var room = CreateRoomWithoutEncounter();
+            Assert.IsFalse(room.IsGuarded(new Repository<Encounter>()));
         }
 
         [Test]
         public void ShouldIndicateItIsGuardedIfThereIsAnUnresolvedEncounter()
         {
+            var encounter = CreateUnresolvedEncounter();
+            var room = CreateRoomWithEncounter(encounter.Id);
+
+            Assert.IsTrue(room.IsGuarded(new Repository<Encounter>(encounter)));
         }
 
         [Test]
         public void ShouldIndicateItIsNotGuardedIfThereIsAResolvedEncounter()
         {
+            var encounter = CreateResolvedEncounter();
+            var room = CreateRoomWithEncounter(encounter.Id);
+
+            Assert.IsFalse(room.IsGuarded(new Repository<Encounter>(encounter)));
+        }
+
+        public static Room CreateRoomWithEncounter(int encounterId)
+        {
+            return new Room(1, "Room", "Room description", encounterId);
+        }
+
+        public static Room CreateRoomWithoutEncounter()
+        {
+            return new Room(1, "Room", "Room description");
         }
 
         public static Encounter CreateUnresolvedEncounter()
